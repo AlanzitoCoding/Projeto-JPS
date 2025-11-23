41f20593ebe19f9e85d85fe27bb493db8cdce41f
alanz <alanzito006@gmail.com>
2025-11-23 14:33:26 -0300

    feat: criação do componente input para estilização padronizada

---
+++ src/app/shared/input/input.component.ts
@@ -0,0 +1,61 @@
+// Louvado seja o Senhor
+
+import { Component, forwardRef, Input } from '@angular/core';
+import { AbstractControl, ControlValueAccessor, FormsModule, NG_VALIDATORS, NG_VALUE_ACCESSOR, ValidationErrors, Validator } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  imports: [FormsModule],
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.css',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true
+    },
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true
+    }
+  ]
+})
+export class InputComponent implements ControlValueAccessor, Validator {
+  @Input() inputType : string = "";
+  @Input() inputName : string = "";
+  @Input() inputID : string = "";
+  @Input() inputPlaceholder : string = "";
+  @Input() required = false;  
+
+  value : any = '';
+
+  onChange = (_: any) => {};
+  onTouched = () => {};
+
+  writeValue(value : any) : void {
+    this.value = value ?? '';
+  }
+
+  registerOnChange(fn : any) : void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn : any) : void {
+    this.onTouched = fn;
+  }
+
+  handleInput(e : Event){
+    const input = e.target as HTMLInputElement;
+    this.value = input.value;
+    this.onChange(this.value);
+    this.onTouched();
+  }
+
+  validate(control : AbstractControl) : ValidationErrors | null {
+    if(this.required && (!control.value || control.value === '')) {
+      return { required: true };
+    }
+    return null;
+  }
+}
